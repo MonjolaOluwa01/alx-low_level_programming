c06d5e215cb7e0dc0e98693ab56cd7f0b6794e4c
Mojolaoluwa Fadahunsi <fadahunsi2018@yahoo.com>
2022-03-10 12:01:37 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of art,"
+ * Description: use printf
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
